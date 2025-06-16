f3eb61096de3ed1a455f2bb733cf5762453df7d2
Moritz Froese <mofro04@gmail.com>
2025-06-16 19:28:55 +0200

    debug

--- train.py
+++ train.py
@@ -151,8 +151,8 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         dL_aux_derror_helper = gaussians.get_e_k.grad                               # E_k_pi
         gaussians.E_k = torch.max(gaussians.E_k, dL_aux_derror_helper)              # set E_k to max(E_k, E_k_pi)
         print("dL_aux_..: \n", dL_aux_derror_helper)
-	print("E_k: ", gaussians.E_k)
-	# log_variable("error_gradient", dL_aux_derror_helper)
+        print("E_k: ", gaussians.E_k)
+	    # log_variable("error_gradient", dL_aux_derror_helper)
         # log_variable("E_k", gaussians.E_k)
         gaussians.e_k.grad.zero_()                                                  # set gradients back to zero after each pass
 
