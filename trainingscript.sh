3812ca4d29da95f39f19893ee56e3eee4e7f2df5
Moritz Froese <s76mfroe_hpc@login01.service>
2025-06-20 10:05:23 +0200

    slight modifications to trainingscript

--- trainingscript.sh
+++ trainingscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --partition=mlgpu_devel
-#SBATCH --time=1:00:00
-#SBATCH --gpus=1
+#SBATCH --partition=mlgpu_short
+#SBATCH --time=3:00:00
+#SBATCH --gpus=2
 #SBATCH --account=ag_ifi_laehner
 #SBATCH --job-name=gs_train
 
@@ -18,4 +18,4 @@ nvcc --version
 
 # Run training
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
-    -s /home/s76mfroe_hpc/nerf-360-scenes/garden
+    -s /home/s76mfroe_hpc/nerf-360-scenes/garden -m output/error-based-densification
