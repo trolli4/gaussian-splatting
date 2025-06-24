1c804cc7b52e9dc5eb473f4ea043c6e901118fca
Moritz Froese <s76mfroe_hpc@login01.service>
2025-06-24 11:10:21 +0200

    changes to eval_...sh

--- eval_threshold.sh
+++ eval_threshold.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=mlgpu_short
 #SBATCH --time=3:00:00
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --account=ag_ifi_laehner
 #SBATCH --job-name=gs_train
 
@@ -9,5 +9,8 @@
 source activate gaussian_splatting
 
 # Run training
-CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
-    -s /home/s76mfroe_hpc/nerf-360-scenes/garden -m output/error-based-densification
+python /home/s76mfroe_hpc/gaussian-splatting/train.py \
+    -s /home/s76mfroe_hpc/nerf-360-scenes/garden \
+    -m output/error-based-densification \
+    --quiet \
+    --eval
