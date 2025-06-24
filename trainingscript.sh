f6459aa7881064040935a789b76e1a1d2c4147b3
Moritz Froese <s76mfroe_hpc@login02.service>
2025-06-24 18:50:06 +0200

    change number of gpus in trainingscript to 1

--- trainingscript.sh
+++ trainingscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=mlgpu_short
 #SBATCH --time=3:00:00
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --account=ag_ifi_laehner
 #SBATCH --job-name=gs_train
 
@@ -18,4 +18,4 @@ nvcc --version
 
 # Run training
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
-    -s /home/s76mfroe_hpc/nerf-360-scenes/garden -m output/error-based-densification
+    -s /home/s76mfroe_hpc/nerf-360-scenes/garden
