f13c805e1f3829b20abba113c8fd94d6f381b5bc
Moritz Froese <s76mfroe_hpc@login02.service>
2025-07-02 01:29:31 +0200

    test Loss and PSNR for {0.1, 0.2, .., 1.0}. change default value of densify_error_threshold from '20' to '20.0' so it can take float values

--- eval_threshold.sh
+++ eval_threshold.sh
@@ -2,8 +2,9 @@
 [ ! -d "./logs/" ] && mkdir "./logs/"
 
 # Loop over threshold values from 20 to 110 in steps of 10
-for threshold in {20..110..10}
+for counter in {1..10..1}
 do
+    threshold=$(echo "scale=2; $counter / 10" | bc)
     # Create a unique job script for this threshold
     JOB_SCRIPT="run_threshold_${threshold}.sh"
 
@@ -13,8 +14,8 @@ do
 #SBATCH --time=3:00:00
 #SBATCH --gpus=1
 #SBATCH --account=ag_ifi_laehner
-#SBATCH --job-name=gs_train_${threshold}
-#SBATCH --output=logs/garden_${threshold}.out
+#SBATCH --job-name=gs_train_${counter}
+#SBATCH --output=logs/garden_${counter}.out
 
 # Activate environment
 source activate gaussian_splatting
@@ -22,16 +23,16 @@ source activate gaussian_splatting
 # Run training with custom threshold
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \\
     -s /home/s76mfroe_hpc/nerf-360-scenes/garden \\
-    -m output/garden_${threshold} \\
+    -m output/garden_${counter} \\
     --quiet \\
     --eval \\
     --densify_error_threshold ${threshold}
 
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/render.py \
-    -m output/garden_${threshold}
+    -m output/garden_${counter}
 
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/metrics.py \
-    -m output/garden_${threshold}
+    -m output/garden_${counter}
 EOF
 
     # Submit the job
