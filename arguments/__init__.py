599ac7e94f2e9ca64acc4275ff35cd7f93f9ea77
Moritz Froese <s76mfroe_hpc@login01.service>
2025-07-04 13:33:49 +0200

    revert 20 -> 20.0 (int to float) change

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -93,7 +93,7 @@ class OptimizationParams(ParamGroup):
         self.densify_from_iter = 500
         self.densify_until_iter = 15_000
         self.densify_grad_threshold = 0.0002
-        self.densify_error_threshold = 20.0
+        self.densify_error_threshold = 20
         self.depth_l1_weight_init = 1.0
         self.depth_l1_weight_final = 0.01
         self.random_background = False
