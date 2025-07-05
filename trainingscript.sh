baa54b37fa104656132da8389211d62faf680dd2
Moritz Froese <s76mfroe_hpc@login02.service>
2025-07-05 17:30:40 +0200

    change 'source activate..' line in trainingscript to use 'source ...conda.sh' 'conda activate ..'

--- trainingscript.sh
+++ trainingscript.sh
@@ -11,8 +11,11 @@ for i in $(seq 2000 1000 30000); do
      iterations_to_test+=" $i"
 done
 
+# Source conda.sh to enable 'conda activate' in this script
+source $(conda info --base)/etc/profile.d/conda.sh
+
 # Activate environment
-source activate gaussian_splatting
+conda activate gaussian_splatting
 
 # Run training
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
