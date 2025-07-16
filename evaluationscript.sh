13204de7bd4aca0d62bcc57705a563c67fea7dd3
Moritz Froese <s76mfroe_hpc@login02.service>
2025-07-16 15:14:04 +0200

    submodule update, add debugging of NaN in train/render

---
+++ evaluationscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=mlgpu_devel
+#SBATCH --time=1:00:00
+#SBATCH --gpus=2
+#SBATCH --account=ag_ifi_laehner
+#SBATCH --job-name=gs_train
+
+# Activate environment
+source activate gaussian_splatting
+
+# debug
+which python
+python -c "import torch; print(torch.cuda.is_available())"
+python -c "import torch; print(torch.__version__, torch.version.cuda)"
+module load CUDA/11.8.0
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+nvcc --version
+
+# Run training
+CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
+    -s /home/s76mfroe_hpc/nerf-360-scenes/garden -m output/error-based-densification
