2174e9d29dc66a9538e18436a02554a2b2440bf3
Moritz Froese <s76mfroe_hpc@login02.service>
2025-07-16 15:06:45 +0200

    add debugging and evaluation to working original - everything fine!

--- evaluationscript.sh
+++ evaluationscript.sh
@@ -11,3 +11,9 @@ source activate gaussian_splatting_old
 # Run training
 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
     -s /home/s76mfroe_hpc/nerf-360-scenes/garden -m output/original-3dgs-test-NaN -r 8
+
+python /home/s76mfroe_hpc/gaussian-splatting/render.py \
+    -m output/original-3dgs-test-NaN
+
+python /home/s76mfroe_hpc/gaussian-splatting/metrics.py \
+    -m output/original-3dgs-test-NaN
