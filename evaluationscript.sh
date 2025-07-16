a876a55703fc8c3b2d66afde955eb57158a77890
Moritz Froese <s76mfroe_hpc@login02.service>
2025-07-16 15:41:53 +0200

    add evaluationscript

--- evaluationscript.sh
+++ evaluationscript.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 #SBATCH --partition=mlgpu_devel
 #SBATCH --time=1:00:00
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --account=ag_ifi_laehner
-#SBATCH --job-name=gs_train
+#SBATCH --job-name=gs_debug_NaN
+
+MODEL_PATH="output/debug_NaN_densification"
+
+# Source conda.sh to enable 'conda activate' in this script
+source $(conda info --base)/etc/profile.d/conda.sh
 
 # Activate environment
-source activate gaussian_splatting
+conda activate gaussian_splatting
 
-# debug
-which python
-python -c "import torch; print(torch.cuda.is_available())"
-python -c "import torch; print(torch.__version__, torch.version.cuda)"
-module load CUDA/11.8.0
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-nvcc --version
-
-# Run training
+echo "trainng.."
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
-    -s /home/s76mfroe_hpc/nerf-360-scenes/garden -m output/error-based-densification
+    -s /home/s76mfroe_hpc/nerf-360-scenes/garden \
+    -m "$MODEL_PATH" \
+    --eval \
+    -r 8
+
+echo "rendering.."
+CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/render.py \
+    -m "$MODEL_PATH" 
+
+echo "evaluating.."
+CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/metrics.py \
+    -m "$MODEL_PATH" 
