64446fb3907b531c73c7112163b20b4621e43ea8
Moritz Froese <mofro04@gmail.com>
2025-07-17 17:18:32 +0200

    add debug of pipeline in train.py

--- train.py
+++ train.py
@@ -328,6 +328,7 @@ if __name__ == "__main__":
     """ if not args.disable_viewer:
         network_gui.init(args.ip, args.port) """
     torch.autograd.set_detect_anomaly(args.detect_anomaly)
+    print("Pipeline:", pp.extract(args))
     training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from)
 
     # All done
