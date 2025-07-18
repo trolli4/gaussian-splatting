ff8fe1ed99abae6fd78dd29c13e82911e36937ca
Moritz Froese <mofro04@gmail.com>
2025-07-18 16:36:28 +0200

    residual opacities now carried up to train.py

--- train.py
+++ train.py
@@ -109,7 +109,10 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         bg = torch.rand((3), device="cuda") if opt.random_background else background
 
         render_pkg = render(viewpoint_cam, gaussians, pipe, bg, use_trained_exp=dataset.train_test_exp, separate_sh=SPARSE_ADAM_AVAILABLE)
-        image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
+        image, viewspace_point_tensor, visibility_filter, radii, residual_opacity = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"], render_pkg["residual_opacity"]
+
+        # debug shape of residual opacity
+        print("residual_opacity shape:", residual_opacity.shape)
 
         if viewpoint_cam.alpha_mask is not None:
             alpha_mask = viewpoint_cam.alpha_mask.cuda()
