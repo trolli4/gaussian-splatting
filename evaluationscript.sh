604b7ec86a1d646052fa5801553165fde1858a41
Moritz Froese <s76mfroe_hpc@login01.service>
2025-08-08 10:15:34 +0200

    add evaluationscript

---
+++ evaluationscript.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --partition=mlgpu_devel
+#SBATCH --time=1:00:00
+#SBATCH --gpus=1
+#SBATCH --account=ag_ifi_laehner
+#SBATCH --job-name=gs_op_reset
+#SBATCH --output=logs/counter_opacity_reset_only_eval.out
+
+MODEL_PATH="counter_opacity_reset_only_eval"
+
+# fill test_iterations with all iterations to compute PSNR at
+iterations_to_test="1000"
+for i in $(seq 2000 1000 30000); do
+     iterations_to_test+=" $i"
+done
+
+# Source conda.sh to enable 'conda activate' in this script
+source $(conda info --base)/etc/profile.d/conda.sh
+
+# Activate environment
+conda activate gaussian_splatting_opacity_reset_only
+
+# Run training
+CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train.py \
+    -s /home/s76mfroe_hpc/nerf-360-scenes/counter \
+    -m "${MODEL_PATH}" \
+    --test_iterations $iterations_to_test \
+    -r 8 \
+    --disable_viewer \
+    --eval
+
+CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/render.py \
+    -m "${MODEL_PATH}"
+
+CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/metrics.py \
+    -m "${MODEL_PATH}"
