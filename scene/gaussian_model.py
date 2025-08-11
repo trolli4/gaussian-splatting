0e484523aded84604b19fe65f33ec22c2fa6f697
Moritz Froese <mofro04@gmail.com>
2025-08-11 12:49:21 +0200

    move opacity update from parameter to sigmoid space; alpha lives here

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -261,6 +261,12 @@ class GaussianModel:
         optimizable_tensors = self.replace_tensor_to_optimizer(opacities_new, "opacity")
         self._opacity = optimizable_tensors["opacity"]
 
+    def correct_opacity(self, mask):
+        alpha = self.opacity_activation(self._opacity[mask])
+        alpha_new = 1.0 - torch.sqrt(1.0 - alpha)                                           # equation from local revising densification paper: alpha_new = 1 - sqrt(1 - alpha_old)
+        opacity_new = self.inverse_opacity_activation(alpha_new / (1.0 - alpha_new))
+        self._opacity[mask] = opacity_new
+
     def load_ply(self, path, use_train_test_exp = False):
         plydata = PlyData.read(path)
         if use_train_test_exp:
@@ -442,8 +448,8 @@ class GaussianModel:
         new_xyz = self._xyz[selected_pts_mask]
         new_features_dc = self._features_dc[selected_pts_mask]
         new_features_rest = self._features_rest[selected_pts_mask]
-        self._opacity[selected_pts_mask] = 1 - torch.sqrt(1 - self._opacity[selected_pts_mask])         # equation from local revising densification paper: 
-        new_opacities = self._opacity[selected_pts_mask]                                                # alpha_new = 1 - sqrt(1 - alpha)
+        self.correct_opacity(selected_pts_mask)                         # alpha_new = 1 - sqrt(1 - alpha_old) // calculated in sigmoid space
+        new_opacities = self._opacity[selected_pts_mask]
         new_scaling = self._scaling[selected_pts_mask]
         new_rotation = self._rotation[selected_pts_mask]
 
