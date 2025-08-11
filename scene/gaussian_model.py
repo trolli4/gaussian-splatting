240692dec878d97f630704a54b0a69de72899b24
Moritz Froese <mofro04@gmail.com>
2025-08-11 13:01:47 +0200

    add 'self.inverse_opacity_activation' so that opacity (parameter space) is not overwritten by alpha (sigmoid space)

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -259,7 +259,7 @@ class GaussianModel:
     def reset_opacity(self):
         opacity_reduction = 0.001
         opacity_minimum = 0.001
-        opacities_new = torch.max(self.get_opacity - opacity_reduction, torch.ones_like(self.get_opacity)*opacity_minimum)
+        opacities_new = self.inverse_opacity_activation(torch.max(self.get_opacity - opacity_reduction, torch.ones_like(self.get_opacity)*opacity_minimum))
         optimizable_tensors = self.replace_tensor_to_optimizer(opacities_new, "opacity")
         self._opacity = optimizable_tensors["opacity"]
 
