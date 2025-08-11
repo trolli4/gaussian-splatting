05924e98d8a09ad8ab3545d8ba96d83670035836
Moritz Froese <mofro04@gmail.com>
2025-08-11 15:16:45 +0200

    add hyperparam for minimum opacity threshold // stop resetting opacity once this threshold is reached

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -93,6 +93,7 @@ class OptimizationParams(ParamGroup):
         self.densify_from_iter = 500
         self.densify_until_iter = 27_000
         self.densify_grad_threshold = 0.0002
+        self.opacity_min_threshold = 0.005
         self.depth_l1_weight_init = 1.0
         self.depth_l1_weight_final = 0.01
         self.random_background = False
--- train.py
+++ train.py
@@ -175,10 +175,10 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
                 if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:
                     size_threshold = 20 if iteration > opt.opacity_reset_interval else None
-                    gaussians.densify_and_prune(opt.densify_grad_threshold, 0.005, scene.cameras_extent, size_threshold, radii)
+                    gaussians.densify_and_prune(opt.densify_grad_threshold, opt.opacity_min_threshold, scene.cameras_extent, size_threshold, radii)
                 
                 if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
-                    gaussians.reset_opacity()
+                    gaussians.reset_opacity(opt.opacity_min_threshold)
 
             # Optimizer step
             if iteration < opt.iterations:
