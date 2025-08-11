997851eecf8b5521fdf8711b34c6b130078da79e
Moritz Froese <mofro04@gmail.com>
2025-08-11 15:03:55 +0200

    apply changes to hyper-parameters introduced by authors

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -91,7 +91,7 @@ class OptimizationParams(ParamGroup):
         self.densification_interval = 100
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
-        self.densify_until_iter = 15_000
+        self.densify_until_iter = 27_000
         self.densify_grad_threshold = 0.0002
         self.depth_l1_weight_init = 1.0
         self.depth_l1_weight_final = 0.01
