ef0705900aab1deb31926b739a1ee46ce1331ace
Moritz Froese <s76mfroe_hpc@login02.service>
2025-09-01 20:36:06 +0200

    add variables for scene path to evaluationscript

--- evaluationscript.sh
+++ evaluationscript.sh
@@ -3,10 +3,13 @@
 #SBATCH --time=3:00:00
 #SBATCH --gpus=1
 #SBATCH --account=ag_ifi_laehner
-#SBATCH --job-name=gs_eval
-#SBATCH --output=logs/flowers_full_eval.out
+#SBATCH --job-name=gs_{$1}
+# #SBATCH --output=logs/own-scenes/eval/{$1}.out
 
-MODEL_PATH="output/flowers_full_eval"
+MY_PATH="own-scenes/$1"
+SCENE_FOLDER=${MY_PATH%/*}
+SCENE=${MY_PATH#*/}
+MODEL_PATH="output/${SCENE_FOLDER}/eval/${SCENE}"
 
 # fill test_iterations with all iterations to compute PSNR at
 iterations_to_test="1000"
@@ -22,7 +25,7 @@ conda activate gaussian_splatting_full
 
 # Run training
 CUDA_LAUNCH_BLOCKING=1 python /home/s76mfroe_hpc/gaussian-splatting/train_render_metrics.py \
-    -s /home/s76mfroe_hpc/nerf-360-scenes/flowers \
+    -s /home/s76mfroe_hpc/"${MY_PATH}" \
     -m "${MODEL_PATH}" \
     --test_iterations $iterations_to_test \
     -r 8 \
