ae448c122a0c7b26ba28885e510e2ad89d99bcc3
Moritz Froese <s76mfroe_hpc@login01.service>
2025-09-09 17:44:00 +0200

    Revert "reset opacity every iteration"
    This reverts commit 2f538491afba666a795313d4dbe213654e44a20d.

--- train.py
+++ train.py
@@ -177,8 +177,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                     size_threshold = 20 if iteration > opt.opacity_reset_interval else None
                     gaussians.densify_and_prune(opt.densify_grad_threshold, opt.opacity_min_threshold, scene.cameras_extent, size_threshold, radii)
                 
-                # slightly decrease opacity each iteration
-                if iteration > opt.densify_from_iter:
+                if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
                     gaussians.reset_opacity(opt.opacity_min_threshold)
 
             # Optimizer step
