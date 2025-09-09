e442ef06155dcef150631f689c01ea25283f3bc8
Moritz Froese <mofro04@gmail.com>
2025-09-09 21:44:18 +0200

    add debug prints

--- train.py
+++ train.py
@@ -112,8 +112,12 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
         bg = torch.rand((3), device="cuda") if opt.random_background else background
 
-        old_grads = gaussians.xyz_gradient_accum / gaussians.denom
-        old_grads[old_grads.isnan()] = 0.0
+        if (iteration in saving_iterations):
+            old_grads = gaussians.xyz_gradient_accum / gaussians.denom
+            old_grads[old_grads.isnan()] = 0.0
+            print("============ old grads (min, max) ===============")
+            print("min:", old_grads.min())
+            print("max:", old_grads.max())
 
         render_pkg = render(viewpoint_cam, gaussians, pipe, bg, use_trained_exp=dataset.train_test_exp, separate_sh=SPARSE_ADAM_AVAILABLE)
         image, viewspace_point_tensor, visibility_filter, radii, error_render = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"], render_pkg["error_render"]
@@ -168,10 +172,16 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         # Reset grads on e_k since we only used them as helpers
         gaussians.e_k.grad.zero_()
 
-        new_grads = gaussians.xyz_gradient_accum / gaussians.denom
-        new_grads[new_grads.isnan()] = 0.0
-
-        grads = new_grads - old_grads
+        if (iteration in saving_iterations):
+            new_grads = gaussians.xyz_gradient_accum / gaussians.denom
+            new_grads[new_grads.isnan()] = 0.0
+            grads = new_grads - old_grads
+            print("============ new grads (min, max) ===============")
+            print("min:", new_grads.min())
+            print("max:", new_grads.max())
+            print("============ grads (min, max) ===============")
+            print("min:", grads.min())
+            print("max:", grads.max())
 
         iter_end.record()
 
