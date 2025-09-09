a64286e56a3a526d11860f1762070ac00a6a4ff8
Moritz Froese <s76mfroe_hpc@login01.service>
2025-09-09 23:16:42 +0200

    invert loss image to match color scheme of gradients

--- train.py
+++ train.py
@@ -199,6 +199,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                 torchvision.utils.save_image(image, os.path.join(render_path, '{0:05d}'.format(iteration) + "_render.png"))
                 torchvision.utils.save_image(gt_image, os.path.join(render_path, '{0:05d}'.format(iteration) + "_gt.png"))
                 loss_image = torch.abs(image - gt_image)
+                loss_image = 1 - loss_image
                 torchvision.utils.save_image(loss_image, os.path.join(render_path, '{0:05d}'.format(iteration) + "_loss.png"))
 
             # Progress bar
