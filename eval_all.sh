0619967b56c8c6c65c48c849830b4cb3b5e25353
Moritz Froese <s76mfroe_hpc@login01.service>
2025-09-11 22:24:18 +0200

    change num of gpus to 1

--- eval_all.sh
+++ eval_all.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Base path where scene folders are located
-BASE_DATASET_PATH="/home/s76mfroe_hpc/nerf-360-scenes"
+BASE_DATASET_PATH="/home/s76mfroe_hpc/own-scenes"
 
 # Log directory
 LOG_DIR="./logs"
 mkdir -p "$LOG_DIR"
 
-scenes="bicycle flowers garden"
+scenes="doll person roundabout castle"
 
 # Loop over all folders in dataset path
 for folder in $scenes; do
     if [ -d "$BASE_DATASET_PATH"/"$folder" ]; then
         folder_name=$(basename "$folder")
-        log_file="${LOG_DIR}/original/eval/${folder_name}.out"
-        model_path="output/original/eval/${folder_name}"
+        log_file="${LOG_DIR}/original/eval_4/${folder_name}.out"
+        model_path="output/original/eval_4/${folder_name}"
 
         sbatch <<EOF
 #!/bin/bash
 #SBATCH --partition=mlgpu_short
 #SBATCH --time=5:00:00
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --account=ag_ifi_laehner
 #SBATCH --job-name=gs_train_${folder_name}
 #SBATCH --output=${log_file}
@@ -28,14 +28,14 @@ for folder in $scenes; do
 source \$(conda info --base)/etc/profile.d/conda.sh
 conda activate gaussian_splatting_old
 
-: <<'comment'
+#: <<'comment'
 python train.py \\
     -s "${BASE_DATASET_PATH}/${folder_name}" \\
     -m "${model_path}" \\
     --disable_viewer \\
     -r -1 \\
     --eval
-comment
+#comment
 
 python render.py \\
     -m "${model_path}"
