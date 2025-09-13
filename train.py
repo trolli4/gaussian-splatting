540166acbd167cdf267ee8629dcf2673963b30b7
trolli4 <mofro04@gmail.com>
2025-09-13 16:12:17 +0200

    save loss as gray-scale image instead of rgb

--- train.py
+++ train.py
@@ -165,6 +165,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                 torchvision.utils.save_image(gt_image, os.path.join(render_path, '{0:05d}'.format(iteration) + "_truth.png"))
                 loss_image = torch.abs(image - gt_image)
                 loss_image = 1 - loss_image
+                loss_image = torchvision.transforms.functional.rgb_to_grayscale(loss_image)
                 torchvision.utils.save_image(loss_image, os.path.join(render_path, '{0:05d}'.format(iteration) + "_loss.png"))
 
             # Progress bar
